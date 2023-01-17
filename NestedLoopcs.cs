421dd0cc32e56381617ac1ed0fd78eb7a5468d10
shraddha2110 <shraddhargaikwad21@gmail.com>
2023-01-17 16:49:46 +0530

    first commit

--- NestedLoopcs.cs
+++ NestedLoopcs.cs
@@ -135,16 +135,16 @@ namespace ConsoleApp1
         //  *      *    *
         //*    *     *     *
 
-        static void Main(string[] args)
-        {
-            for (int i = 1; i <= 4; ++i)
-            {
-                for (int j = 1; j <=i; ++j)
-                {
-                    Console.Write("  *  ");
-                }
-                Console.WriteLine();
-            }
-        }
+        //static void main(string[] args)
+        //{
+        //    for (int i = 1; i <= 4; ++i)
+        //    {
+        //        for (int j = 1; j <=i; ++j)
+        //        {
+        //            console.write("  *  ");
+        //        }
+        //        console.writeline();
+        //    }
+        //}
     }
 }
